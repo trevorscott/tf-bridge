0be36505cafe788be346c241e97a864723c25ef7
Trevor Scott <tscott@heroku.com>
2018-04-16 13:59:23 -0700

    unzip model to generic name

--- .profile.d/download-model.sh
+++ .profile.d/download-model.sh
@@ -3,5 +3,9 @@ mkdir -p /app/.tf-model
 cd /app/.tf-model
 curl $TENSORFLOW_MODEL_URL --output default.tar.gz
 tar -zxvf default.tar.gz
+
+OLD_DIR_NAME="$(ls -I default.tar.gz)"
+mv $OLD_DIR_NAME default
+
 cd /app
 
